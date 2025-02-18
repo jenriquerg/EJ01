0aa61beb64fb9bed282a5355ced09e01dc62792f
Enrique Rojas <rojasguerrero123@gmail.com>
2025-02-18 11:17:05 -0600

    bug orWhere

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express'); // Para crear el servidor
 const jwt = require('jsonwebtoken'); // Para generar el token
 const bodyParser = require('body-parser'); // Para parsear el cuerpo de la petición
 const cors = require('cors'); // Para permitir peticiones desde cualquier origen
-const bcrypt = require('bcrypt'); // Para hashear la contraseña
-const admin = require("firebase-admin");
 
+const admin = require("firebase-admin");
 const app = express(); // Crear el servidor
 const PORT = 3000; // Puerto donde correrá el servidor
 const SECRET_KEY = 'aguacate'; // Clave secreta para firmar el token
 
+const bcrypt = require('bcrypt'); // Para hashear la contraseña
 let serviceAccount = require("./key/serviceAccountKey.json"); // Credenciales de Firebase
 
 // Inicializar Firebase
@@ -27,9 +27,7 @@ app.use(bodyParser.json());
 
 // Ruta de registro
 app.post('/register', async (req, res) => {
-    //Extraer los datos del cuerpo de la petición sin fechas
     const { email, username, password, role } = req.body;
-    // Verificar que los campos no estén vacíos
     if (!email || !username || !password || !role) {
         return res.status(400).json({
             statusCode: 400,
@@ -38,26 +36,18 @@ app.post('/register', async (req, res) => {
         });
     }
     try {
-        // Buscar si ya existe un usuario con el mismo username o email
         const usersRef = db.collection('users');
-        const querySnapshot = await usersRef
-            .where('username', '==', username)
-            .orWhere('email', '==', email)
-            .get();
-    
-        if (!querySnapshot.empty) {
+        const usernameSnapshot = await usersRef.where('username', '==', username).get();
+        const emailSnapshot = await usersRef.where('email', '==', email).get();
+        if (!usernameSnapshot.empty || !emailSnapshot.empty) {
             return res.status(400).json({
                 statusCode: 400,
                 message: "Usuario ya existe",
                 intDataMessage: [{ message: "El usuario o el correo ya están registrados" }]
             });
         }
-    
-        // Encriptar la contraseña
         const hashedPassword = await bcrypt.hash(password, 10);
         const timestamp = new Date();
-    
-        // Guardar el usuario en Firestore
         await usersRef.doc(username).set({
             email,
             username,
@@ -66,13 +56,11 @@ app.post('/register', async (req, res) => {
             date_register: timestamp,
             last_login: ""
         });
-    
         res.status(201).json({
             statusCode: 201,
             message: "Usuario registrado exitosamente",
             intDataMessage: [{ message: "Registro completado" }]
         });
-    
     } catch (error) {
         console.error(error);
         res.status(500).json({
@@ -83,10 +71,11 @@ app.post('/register', async (req, res) => {
     }
 });
 
+
+
 // Ruta de inicio de sesión
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    // Verificar que los campos no estén vacíos
     if (!username || !password) {
         return res.status(400).json({
             statusCode: 400,
@@ -94,11 +83,10 @@ app.post('/login', async (req, res) => {
             intDataMessage: [{ message: "Usuario o contraseña no proporcionados" }]
         });
     }
-    // Verificar que el usuario exista
     try {
         const userRef = db.collection('users').doc(username);
         const doc = await userRef.get();
-        // Verificar si el usuario no existe
+
         if (!doc.exists) {
             return res.status(401).json({
                 statusCode: 401,
@@ -106,9 +94,7 @@ app.post('/login', async (req, res) => {
                 intDataMessage: [{ message: "Usuario no encontrado" }]
             });
         }
-        // Extraer los datos del usuario
         const userData = doc.data();
-        //Verificar la contraseña hasheada
         const isMatch = await bcrypt.compare(password, userData.password);
         if (!isMatch) {
             return res.status(401).json({
@@ -117,18 +103,13 @@ app.post('/login', async (req, res) => {
                 intDataMessage: [{ message: "Credenciales incorrectas" }]
             });
         }
-        // Actualizar la fecha de último inicio de sesión
         await userRef.update({ last_login: new Date() });
-
-        //Generar token con la información del usuario
         const token = jwt.sign({ username: userData.username, role: userData.role }, SECRET_KEY, { expiresIn: '1m' });
-
         res.status(200).json({
             statusCode: 200,
             message: "OK",
             intDataMessage: [{ credentials: token }]
         });
-
     } catch (error) {
         console.error(error);
         res.status(500).json({
@@ -138,6 +119,7 @@ app.post('/login', async (req, res) => {
         });
     }
 });
+
 
 // Middleware de error 404 para rutas no encontradas
 app.use((req, res) => {
